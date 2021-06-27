f97812b2469e2ad76843f9109085e2d48c67d181
dev3 <nik.shvedov.2018@mail.ru>
2021-06-27 12:23:42 +0300

    Сделать запросы: для поиска товара и для получения данных конкретного товара, используя fetch и await

--- src/utils/api.ts
+++ src/utils/api.ts
@@ -1,4 +1,35 @@
-// тут можно складировать функции для запросов к апи
-export const fetchData = () => {
-  return 'something';
+import Results from "../components/Results/Results";
+import { Instance } from 'mobx-state-tree';
+
+let url = `https://api.punkapi.com/v2`;
+
+export const getBeers = async (page: number) => {
+  const response = await fetch(url + `/beers?page=${page}`);
+  const json = (await response.json()) as Instance<typeof Results>[];
+  return json;
 };
+
+export const getBeer = async (id: number) => {
+  const response = await fetch(url + `/beers/${id}`);
+  const json = (await response.json()) as Instance<typeof Results>[];
+  return json;
+};
+
+/*
+export const fetchData = {
+
+  async fetchRandomBeer({ commit }: any) {
+    const response = await fetch(url + `/beers/random`);
+    const data = await response.json();
+    commit('setRandomBeer', data)
+    console.log('setRandomBeer', data[0]);
+  },
+  
+  async fetchBeerById({ commit }: any, id: number) {
+    let response = await fetch(url + `/beers/${id}`);
+    const data = await response.json();
+    commit('setBeer', data)
+    console.log('setBeer', data);
+  }
+}
+*/
